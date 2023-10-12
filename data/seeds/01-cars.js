d4d76269e19741cd4a316c8138f6e022de037011
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-12 14:13:08 -0700

    migration completed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567890123',
+        make: 'toyota',
+        model: 'camry',
+        mileage: 100000,
+        title: 'clean',
+        transmision: 'automatic',
+    },
+    {
+        vin: '1234567891123',
+        make: 'honda',
+        model: 'civic',
+        mileage: 135000,
+        title: 'clean',
+        transmision: 'manual',
+    },
+    {
+        vin: '1234567893333',
+        make: 'BMW',
+        model: '540',
+        mileage: 40000,
+        transmision: 'automatic',
+    }
+]
+
+exports.seed = function(knex){
+    return knex('knex')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    }) 
+}
